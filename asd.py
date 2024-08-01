6f3f126075f4d9198c3084fc7a68ea014c401d0a
kauã cesar <kauacesarpereira049@gmail.com>
2024-08-01 15:13:11 -0300

    Create asd.py

---
+++ asd.py
@@ -0,0 +1,8 @@
+print("faça login")
+no1 = input("digite o nome:")
+s1 = input("digite a senha:")
+
+if no1 == "procopio" and s1 == "12345" or no1 == "paiva" and s1 == "54321":
+  print("Seja bem vindo!")
+else:
+  print("Usuário e senha não conferem!!! :(")
